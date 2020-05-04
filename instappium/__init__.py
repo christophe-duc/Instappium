7ce0d17448b399d56a70f1ac49bbd2eb36505c97
christophe-duc <christophe@twilight-corp.com>
2020-05-04 11:10:58 -0300

    bump version

--- instappium/__init__.py
+++ instappium/__init__.py
@@ -1,8 +1,8 @@
 # flake8: noqa
 
 # __variables__ with double-quoted values will be available in setup.py
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 from .instappium import InstAppium
-from .engine import FSMSession
+from .engine.fsm import FSMSession
 from .common import Settings
